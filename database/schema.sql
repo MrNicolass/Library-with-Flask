cc8de340446e4be914467fc3d51ab2b118b11a8e
Nicolas Gustavo Conte <nicolascontegt@gmail.com>
2024-09-20 21:23:34 -0300

    Feat: User Exhibition and Validations
    - User registration has validations of if login is already created or if it's a valid e-mail address;
    - There's a new page called "users" to bring in a table all created users;
    - Some CSS ajustments.

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
   id integer PRIMARY KEY autoincrement,
   login varchar(50) NOT NULL,
-  password varchar(30) NOT NULL,
+  password varchar(100) NOT NULL,
   firstName varchar(80) NOT NULL,
   lastName varchar(20) NOT NULL,
   status integer NOT NULL DEFAULT 1,
--- routes/users.py
+++ routes/users.py
@@ -2,14 +2,12 @@ from flask import render_template, request, jsonify
 from . import bp
 from database.dbFunctions import get_db
 import re
+from bcrypt import *
 
 #Funcion if it is a valid email
 def email_validation(email):
-    padrao = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-    if re.match(padrao, email):
-        return True
-    else:
-        return False
+    pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return bool(re.match(pattern, email))
 
 @bp.route('/login')
 def login():
@@ -19,32 +17,87 @@ def login():
 def register():
     return render_template("register.html")
 
-@bp.route('/users', methods=['GET', 'POST'])
+@bp.route('/users', methods=['GET', 'POST', 'DELETE'])
 def users():
     if request.method == 'GET':
-        ...
+        return get_users()
     elif request.method == 'POST':
         return create_user()
+    elif request.method == 'DELETE':
+        return block_user()
 
-def create_user():
+def get_users():
     try:
+        #Database connection handling
         db = get_db()
         cursor = db.cursor()
 
+        #Getting data
+        users = cursor.execute("SELECT * FROM users").fetchall()
+        #musicas_list = [dict(zip([desc[0] for desc in cursor.description], row)) for row in users]
+        return render_template('users.html', dados = users)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+    finally:
+        cursor.close()
+        db.close()
+
+def create_user():
+    try:
+        #Database connection handling
+        db = get_db()
+        cursor = db.cursor()
+
+        #Getting data
         login = request.form['login']
-        password = request.form['password']
+        password = request.form['password'].encode('utf-8')
         firstName = request.form['firstName']
         lastName = request.form['lastName']
+
+        #Validations
+        alreadyExists = cursor.execute(f"SELECT login FROM users WHERE LOWER(login) = LOWER('{login}')").fetchone()
+
+        if alreadyExists:
+            return jsonify({"error": "Usuário já existe!"}), 400
 
         if not email_validation(login):
             return jsonify({"error": "Não é um e-mail Válido!"}), 400
 
         if not login or not password or not firstName or not lastName:
             return jsonify({"error": "Preencha todos os campos"}), 400
-        else:
-            cursor.execute(f"INSERT INTO users (login, password, firstName, lastName) VALUES ('{login}', '{password}', '{firstName}', '{lastName}')")
-            db.commit()
-            return jsonify({"message": "Usuário criado com sucesso!"}), 201
+        
+        passHash = hashpw(password, gensalt()).decode('utf-8')
+        
+        cursor.execute(f"INSERT INTO users (login, password, firstName, lastName) VALUES ('{login}', '{passHash}', '{firstName}', '{lastName}')")
+        db.commit()
+        return jsonify({f"message": "Usuário criado com sucesso!"}), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+    finally:
+        cursor.close()
+        db.close()
+
+def block_user():
+    try:
+        #Database connection handling
+        db = get_db()
+        cursor = db.cursor()
+
+        #Getting data
+        id = request.form['id']
+
+        #Validation
+        userExists = cursor.execute(f"SELECT id FROM users WHERE id = '{id}'").fetchone()
+        if not userExists:
+            return jsonify({"error": "Usuário não existe!"}), 400
+
+        cursor.execute(f"UPDATE FROM users SET status = 0 WHERE id = '{id}'")
+        db.commit()
+        return render_template('users.html')
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
